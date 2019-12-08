3cea3e048dede592a6f3a83d38ba1a4a0f4bcf19
Raghu-M <raghum112233@gmail.com>
2019-12-08 18:05:07 +0530

    bank2 commit

--- src/main/java/com/hcl/hclBank/AccountRepository.java
+++ src/main/java/com/hcl/hclBank/AccountRepository.java
@@ -7,5 +7,8 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface AccountRepository extends CrudRepository<Account, Integer>{
 
+public List<Account> findAllByuserid(int id); 
+
+
 
 }
--- src/main/java/com/hcl/hclBank/BankService.java
+++ src/main/java/com/hcl/hclBank/BankService.java
@@ -3,5 +3,11 @@ package com.hcl.hclBank;
 import java.util.List;
 
 public interface BankService {
+
+	public List<Account> getAll(int userid);
+	public String create(Account acc);
+	public String credit(Account acc);
+	public String debit(Account acc);
+
 	
 }
